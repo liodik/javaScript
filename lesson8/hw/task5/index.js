462768186bb93cc2aa63da0c231fba00297656d8
Volodymyr Meduna <liodik89@gmail.com>
2020-12-10 14:19:24 +0200

    commit

--- lesson8/hw/task5/index.js
+++ lesson8/hw/task5/index.js
@@ -1,10 +1,7 @@
-const withdraw = (clients, balances, client, amount) => {
-  const remaind = balances[clients.indexOf(client)] - amount;
-  if (remaind < 0) {
-    return -1;
-  }
-  balances[clients.indexOf(client)] = remaind;
-  return remaind;
-};
+function mergeObjectsV1(obj1, obj2) {
+  return Object.assign(obj1, obj2);
+}
 
-console.log(withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'Ann', 100));
+function mergeObjectsV2(obj1, obj2) {
+  return Object.assign(obj2, obj1);
+}
