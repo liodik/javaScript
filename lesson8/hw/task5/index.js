6dad1e38a9d06433a944b22deee89e52adabc825
Volodymyr Meduna <liodik89@gmail.com>
2020-12-10 14:36:32 +0200

    task5

--- lesson8/hw/task5/index.js
+++ lesson8/hw/task5/index.js
@@ -1,7 +1,19 @@
 function mergeObjectsV1(obj1, obj2) {
-  return Object.assign(obj1, obj2);
+  return Object.assign({}, obj1, obj2);
 }
 
+// const obj1 = { 1: 2, 3: 4 };
+// const obj2 = { 1: 6, 7: 8 };
+// mergeObjectsV1(obj1, obj2);
+
 function mergeObjectsV2(obj1, obj2) {
-  return Object.assign(obj2, obj1);
+  return Object.assign({}, obj2, obj1);
+}
+
+function mergeObjectsV3(obj1, obj2) {
+  return { ...obj1, ...obj2 };
+}
+
+function mergeObjectsV4(obj1, obj2) {
+  return { ...obj2, ...obj1 };
 }
