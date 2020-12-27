0d240e5ac1c046c3b7af456879b68c13642d6f23
Volodymyr Meduna <liodik89@gmail.com>
2020-12-27 12:30:00 +0200

    c

---
+++ lesson21/hw/task5/index.js
@@ -0,0 +1,11 @@
+export const setButton = buttonText => {
+  document.querySelector('body').innerHTML = `<button>${buttonText}</button>`;
+};
+
+// setButton('javascript');
+
+// const setButton2 = buttonText => {
+//   document.querySelector('body').textContent = `<button>${buttonText}</button>`;
+// };
+
+// setButton2('javascript');
