deffac5f07d198f7d75ce4e5d6a3dde48e54e6a9
Volodymyr Meduna <liodik89@gmail.com>
2020-12-27 23:05:56 +0200

    commit

--- lesson22/hw/task8/index.js
+++ lesson22/hw/task8/index.js
@@ -1,17 +1,7 @@
-export const finishList = () => {
-  const listItem1 = document.createElement('li');
-  listItem1.textContent = '1';
-  document.querySelector('ul').prepend(listItem1);
+const checkboxElem = document.querySelector('.task-status');
 
-  const listItem4 = document.createElement('li');
-  listItem4.textContent = '4';
-  document.querySelector('.special').before(listItem4);
-
-  const listItem6 = document.createElement('li');
-  listItem6.textContent = '6';
-  document.querySelector('.special').after(listItem6);
-  const listItem8 = document.createElement('li');
-  listItem8.textContent = '8';
-  document.querySelector('ul').append(listItem8);
+const checkboxFunc = event => {
+  event.target.checked ? console.log(true) : console.log(false);
 };
-// finishList();
+
+checkboxElem.addEventListener('change', checkboxFunc);
