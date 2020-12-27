dc3bc35c66755ca03a7911d55440e356848682a9
Volodymyr Meduna <liodik89@gmail.com>
2020-12-27 21:27:44 +0200

    commit

--- lesson22/hw/task1/index.js
+++ lesson22/hw/task1/index.js
@@ -1,14 +1,3 @@
-export const getTitleElement = () => {
-  const titleElement = document.querySelector('.title');
-  console.dir(titleElement);
-  return titleElement;
-};
-
-// console.log(getTitleElement());
-
-export const getInputElement = () => {
-  const inputElement = document.querySelector('input[type=text]');
-  console.dir(inputElement);
-  return inputElement;
-};
-// console.log(getInputElement());
+const buttonEl = document.querySelector('.create-btn');
+const buttonFunc = () => alert('done');
+buttonEl.addEventListener('click', buttonFunc);
