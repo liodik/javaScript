6214eb128f80e28290f4d2c9b644683fa4b8c74c
Volodymyr Meduna <liodik89@gmail.com>
2020-12-27 11:04:28 +0200

    coommit

--- lesson21/hw/task1/index.js
+++ lesson21/hw/task1/index.js
@@ -1,35 +1,7 @@
-export class Order {
-  constructor(price, city, type) {
-    this.price = price;
-    this.city = city;
-    this.type = type;
-  }
+const getTitleElement = () => {
+  const titleElement = document.querySelector('.title');
+  console.dir(titleElement);
+  return titleElement;
+};
 
-  id = (Math.random() * 1000).toFixed();
-
-  dateCreated = new Date();
-
-  isConfirmed = false;
-
-  dateConfirmed = new Date();
-
-  checkPrice() {
-    return this.price > 1000 ? true : false;
-  }
-
-  confirmOrder() {
-    this.isConfirmed = true;
-    this.dateConfirmed = new Date();
-  }
-
-  isValidType() {
-    return this.type === 'Buy' || this.type === 'Sell' ? true : false;
-  }
-}
-
-// const order1 = new Order(1200, 'Lviv', 'Buy');
-// console.log(order1);
-// console.log(order1.checkPrice());
-// console.log(order1.confirmOrder());
-// console.log(order1.dateConfirmed);
-// console.log(order1.isValidType());
+console.log(getTitleElement());
