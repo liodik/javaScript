8e2049d3ec07210220ea9d5315dd4657db605d23
Volodymyr Meduna <liodik89@gmail.com>
2020-12-29 19:35:39 +0200

    commit

---
+++ lesson24/hw/task1/index.js
@@ -0,0 +1,16 @@
+export const getDiff = (startDate, endDate) => {
+  const start = new Date(startDate).getTime();
+
+  const end = new Date(endDate).getTime();
+  const diff = Math.abs(start - end);
+  const oneDay = 1000 * 60 * 60 * 24;
+  const days = Math.floor(diff / oneDay);
+  const oneHour = 1000 * 60 * 60;
+  const hours = Math.floor((diff - days * oneDay) / oneHour);
+  const oneMinute = 1000 * 60;
+  const minutes = Math.floor((diff - days * oneDay - hours * oneHour) / oneMinute);
+  const seconds = Math.floor((diff - days * oneDay - hours * oneHour - minutes * oneMinute) / 1000);
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+};
+
+// console.log(getDiff(new Date(2020, 11, 28, 4, 7, 35, 11), new Date(2020, 11, 29)));
