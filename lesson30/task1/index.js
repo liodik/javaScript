c8c34141eedf333a514defc995c7ed5daac85c5c
Volodymyr Meduna <liodik89@gmail.com>
2021-01-04 21:24:02 +0200

    commit

---
+++ lesson30/task1/index.js
@@ -0,0 +1,18 @@
+const addImage = imgSrc => {
+  const p = new Promise((resolve, regect) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'my photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+      const { width, height } = imgElem;
+      resolve({ width, height });
+    };
+    imgElem.addEventListener('load', onImageLoaded);
+    imgElem.addEventListener('error', () => regect(new Error('Image load is failed...')));
+  });
+  return p;
+};
+addImage();
