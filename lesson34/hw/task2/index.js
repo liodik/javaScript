c88a8b30b753df9a5f87a51e72c68c5f36d43ea0
Volodymyr Meduna <liodik89@gmail.com>
2021-01-12 21:34:52 +0200

    commit

--- lesson34/hw/task2/index.js
+++ lesson34/hw/task2/index.js
@@ -8,16 +8,16 @@ const validation = () => {
   submitBtnElem.disabled = !isValide;
   return submitBtnElem.disabled;
 };
-
 const addNewUser = user => {
   return fetch(baseUrl, {
     method: 'POST',
-    headers: { 'Content-type': 'application/json; charset=utf-8' },
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
     body: JSON.stringify(user),
   });
 };
-
-const onFormSubmit = e => {
+function onFormSubmit(e) {
   e.preventDefault();
   const formData = [...new FormData(formElem)].reduce(
     (acc, [field, value]) => ({ ...acc, [field]: value }),
@@ -34,7 +34,7 @@ const onFormSubmit = e => {
       errorTextElem.textContent = 'Failed to create user';
       submitBtnElem.disabled = true;
     });
-};
+}
 
 formElem.addEventListener('submit', onFormSubmit);
 formElem.addEventListener('input', validation);
