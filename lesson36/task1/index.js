cd5789d8f5f278b774099a0bfbfc8a55748e3e6e
Volodymyr Meduna <liodik89@gmail.com>
2021-01-13 18:22:07 +0200

    commit

---
+++ lesson36/task1/index.js
@@ -0,0 +1,15 @@
+export const fetchUser = async userId => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${userId}`);
+    if (response.status === 404) {
+      return null;
+    }
+    const userData = await response.json();
+    return userData;
+  } catch (err) {
+    throw new Error('Failed to fetch user');
+  }
+};
+
+// fetchUser('f2143').then(userData => console.log(userData));
+// // .catch(err => alert(err.message));
