1d41573287b1b0891f2e8dbec138d316864ac65a
Volodymyr Meduna <liodik89@gmail.com>
2021-01-13 20:11:12 +0200

    commit

---
+++ lesson36/hw/task1/index.js
@@ -0,0 +1,18 @@
+const func = userId => {
+  return fetch(`https://api.github.com/users/${userId}`).then(res => res.json());
+};
+
+// const res = func('facebook');
+// console.log(res);
+export const getUserBlogs = async (...users) => {
+  try {
+    const arrOfPromise = users.map(user => func(user));
+    const res = await Promise.all(arrOfPromise);
+    const blogList = res.map(el => el.blog);
+    return blogList;
+  } catch (err) {
+    return err;
+  }
+};
+
+// getUserBlogs('facebook', 'github', 'google').then(linkList => console.log(linkList));
